5ce66d60d05b611cbac898ee547b09982410c752
Dan Boxler <danbox19@msn.com>
2013-10-22 22:22:22 -0600

    added UI.java and UserManager.java

---
+++ UI.java
@@ -0,0 +1,23 @@
+/*
+ * @file: UI.java
+ * @purpose: User interface for PA2
+ */
+
+public UI
+{
+    //private data members
+    private User currentUser;
+    private MusicManager mMngr;
+    private UserManager uMngr;
+
+    //parses input file to create users on songs
+    private parseFile(String file) //TODO
+    {
+
+    }
+
+    public static void main(String args[]) //TODO
+    {
+
+    }
+}
