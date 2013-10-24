80b38f3615cf37b9025f27e41b641e0d04dd037f
Tyler <tylerbuddy@gmail.com>
2013-10-23 19:04:27 -0600

    add library class

---
+++ src/Library.java
@@ -0,0 +1,39 @@
+/*
+ * @file: Library.java
+ * @purpose: consists of Library properties and actions, including playlist
+ */
+
+import java.util.List;
+
+
+public Library
+{
+        //private data members
+	private List<Song> songs;
+	private List<Library> playlists;
+	private List<Pair<User,int>> friendBarrowLimit
+	
+	//public methods
+
+	//add song to user library
+	public void addSong(Song a) //TODO
+	{
+	
+	}
+	//remove a song from the user library
+	public void removeSong(Song b) //TODO
+	{
+	
+	}
+	//create a playlist for the user based on a selection of songs
+	public void createPlaylist(List<Song> a) //TODO
+	{
+	
+	}
+	//display the user's library based on the string value(artist, song ,album)
+	public List<String> diplayLib() //TODO
+	{
+	
+	}
+	
+}
