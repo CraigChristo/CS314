7f40a7b4ae96adf4ca94c2ad56e001eab8f3803f
Tyler <tylerbuddy@gmail.com>
2013-10-23 19:12:01 -0600

    add song class, we also need to add the standard getters and setters that are not part of the diagram

---
+++ src/Song.java
@@ -0,0 +1,22 @@
+/*
+ * @file: Song.java
+ * @purpose: consists of song meta data
+ */
+
+
+
+
+public Song
+{
+        //private data members
+	private String name;
+	private String artist;
+	private String album;
+	private int year;
+	private String composer;
+	private String Genre;
+	private boolean isBarrowable;
+	private boolean permBarrow;
+	private boolean isAvailable;
+	private User owner;
+}
